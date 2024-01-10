7bcbafabf77e1dd444d81d3ba7a0ca6f3d719e9c
lgazzola <luca.gazzola@lastminute.com>
2024-01-10 20:03:50 +0300

    only showing map and search

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve your index.html file for the root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
-// Serve static files from the "client" directory
 app.use(express.static(path.join(__dirname, 'client')));
 
-// Start the server
 const server = app.listen(5000, 'localhost', () => {
     console.log('Server is listening at localhost on port 5000');
 });
