32761b427591a76b4b89669dc5babfa9e52df95e
lgazzola <luca.gazzola@lastminute.com>
2024-08-07 22:06:32 +0200

    added city search

--- client/app.js
+++ client/app.js
@@ -3,3 +3,19 @@ var map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
+
+function updateMap(city) {
+    var url = 'https://nominatim.openstreetmap.org/search?q=' + encodeURIComponent(city) + '&format=json';
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            var latitude = data[0].lat;
+            var longitude = data[0].lon;
+
+            var marker = L.marker([latitude, longitude]).addTo(map);
+            map.setView([latitude, longitude], 13);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
